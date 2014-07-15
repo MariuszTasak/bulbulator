19af6ad26e09d0b887801388854d0f41f2d452e5
Jonathan Gautheron <jgautheron@users.noreply.github.com>
2014-07-15 17:29:18 +0200

    Update common.sh

--- configs/jenkins/common.sh
+++ configs/jenkins/common.sh
@@ -3,13 +3,13 @@ export MYSQL_PASSWORD="jpDvhbcXLvLu6RHj"
 export MYSQL_DB_PREFIX="jenkins_bbl"
 export MYSQL_DB_HOST="localhost"
 
-export ENV_NAME=prep
+export ENV_NAME="prep"
 
 export BASE_SETUP_DIR_TO_CHECK="/var/www/"
 export BASE_SETUP_DIR=$BASE_SETUP_DIR_TO_CHECK"testing/"
 export MEDIA_DIR="/home/root/mediasync_v2"
-export REPOSITORY_URL=git@github.com:jgautheron/bulbulator
-export SUB_DOMAIN=.build-bbl.nexwai.pl
+export REPOSITORY_URL="git@github.com:jgautheron/bulbulator"
+export SUB_DOMAIN=".build-bbl.nexwai.pl"
 
-export WEBSITE=webstore
-export BRANCH=develop
+export WEBSITE="webstore"
+export BRANCH="develop"
